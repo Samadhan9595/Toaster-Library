17ddd9e1067f1c86920bc1bf48bcfa9cbe30ff03
Samadhan9595 <75772828+Samadhan9595@users.noreply.github.com>
2022-03-16 12:35:41 +0530

    first msg

---
+++ app/src/main/java/com/example/mylibproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mylibproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
